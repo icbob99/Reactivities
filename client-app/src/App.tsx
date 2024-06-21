1eadf5ba88bf684883528d0ec122c68356e22545
Boris Shulman WGT <boriss@waveguardtech.com>
2024-06-21 13:11:32 +0300

    remove demo example

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,16 +1,12 @@
 
 import './App.css'
-import DuckItem from './assets/DuckItem'
-import { ducks } from './demo'
 
 function App() {
 
   return (
     <div className='App'>
       <h1>Reactivities</h1>
-      {ducks.map(duck => (
-        <DuckItem key={duck.name} duck={duck} />
-      ))}
+
     </div>
   )
 }
