ae3a3ec1eda9063e38e07a3de7cbb8e29bfc08ae
Boris Shulman WGT <boriss@waveguardtech.com>
2024-06-29 17:41:06 +0300

    Add React

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,29 @@
+
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Container, Header, List } from 'semantic-ui-react'
+import './styles.css'
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data)
+    })
+  }, [])
+
+  return (
+    <div className='App'>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }} >
+       <ActivityDashboard activities={activities}></ActivityDashboard>
+      </Container>
+    </div>
+  )
+}
+
+export default App
