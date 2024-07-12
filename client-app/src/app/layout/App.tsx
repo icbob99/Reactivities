4da5c176889eec85629bf57cca18dee7567ef9b0
Boris Shulman WGT <boriss@waveguardtech.com>
2024-07-12 18:26:51 +0300

    activityStore

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from 'react'
-import { Button, Container } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import './styles.css'
 import { Activity } from '../models/Activity';
 import NavBar from './NavBar';
@@ -17,21 +17,12 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      });
-
-      setActivities(activities);
-      setLoading(false);
-    })
-  }, [])
+    activityStore.loadActivities();
+    
+  }, [activityStore])
 
   function handleSelectActivity(id: string) {
     setSelectedActivity(activities.find(x => x.id === id))
@@ -81,16 +72,15 @@ function App() {
     })
   }
 
-  if (loading) return (<LoadingComponents content='Loading app' />);
+  if (activityStore.loadingInitial) return (<LoadingComponents content='Loading app' />);
 
   return (
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }} >
-        <h2>{activityStore.title}</h2>
-        <Button content='Add exclamation!' positive onClick={activityStore.setTitle} />
+
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
