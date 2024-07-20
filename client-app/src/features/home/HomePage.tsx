c616528bbff0901dded5f9ae86b6a6e1ec24f98d
Boris Shulman WGT <boriss@waveguardtech.com>
2024-07-20 18:30:36 +0300

    end section of 9

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Container, Header, Segment, Image, Button} from "semantic-ui-react";
 
-export default function HomePage(){
+export default function HomePage() {
     return (
-        <Container style={{marginTop: '7em'}}>
-            <h1>Home Page</h1>
-            <h3>Go To <Link to='/activities'>Activities</Link></h3>
-        </Container>
+        <Segment inverted textAlign="center" vertical className="masthead">
+            <Container text>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}}></Image>
+                    Reactivities
+                </Header>
+                <Header as='h2' inverted>Welcome to Reactivities</Header>
+                <Button as={Link} to='/activities' color='teal' size='huge' inverted >Take me to activities</Button>
+            </Container>
+        </Segment>
     )
 }
