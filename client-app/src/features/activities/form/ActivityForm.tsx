0153615e3aaca2918518d82a395ba2f0a0b25381
Boris Shulman WGT <boriss@waveguardtech.com>
2024-08-16 14:42:09 +0300

    add orm and Field

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -51,14 +51,14 @@ export default observer(function ActivityForm() {
     return (
         <Segment clearing>
             <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
-                {({ values: activity, handleChange, handleSubmit }) => (
+                {({ handleSubmit }) => (
                     <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
-                        <Field placeholder='Title' value={activity.title} name='title' onChange={handleChange} />
-                        <Field placeholder='Description' value={activity.description} name='description' onChange={handleChange} />
-                        <Field placeholder='Category' value={activity.category} name='category' onChange={handleChange} />
-                        <Field type='date' placeholder='Date' value={activity.date} name='date' onChange={handleChange} />
-                        <Field placeholder='City' value={activity.city} name='city' onChange={handleChange} />
-                        <Field placeholder='Venue' value={activity.venue} name='venue' onChange={handleChange} />
+                        <Field placeholder='Title'  name='title'  />
+                        <Field placeholder='Description'  name='description'  />
+                        <Field placeholder='Category'  name='category'  />
+                        <Field type='date' placeholder='Date'  name='date'  />
+                        <Field placeholder='City'  name='city'  />
+                        <Field placeholder='Venue'  name='venue'  />
                         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
