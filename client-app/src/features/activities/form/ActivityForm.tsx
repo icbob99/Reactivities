5f0aaf2106e69e496427b6f4becb1f2816b62824
Boris Shulman WGT <boriss@waveguardtech.com>
2024-08-17 11:09:22 +0300

    add validation to all fields Add new Input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -5,17 +5,17 @@ import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponents from "../../../app/layout/LoaadingComponent";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from 'yup';
+import MyTextInput from "../../../app/common/form/MyTextInput";
 
 
 export default observer(function ActivityForm() {
 
     const { activityStore } = useStore();
 
-    const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore;
+    const {  loading, loadActivity, loadingInitial } = activityStore;
     const { id } = useParams();
-    const navigate = useNavigate();
 
     const [activity, setActivity] = useState<Activity>({
         id: '',
@@ -28,7 +28,12 @@ export default observer(function ActivityForm() {
     });
 
     const validationSchema = Yup.object({
-        title: Yup.string().required('The activity title is required')
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required('The activity category is required'),
+        date: Yup.string().required('The activity date is required'),
+        city: Yup.string().required('The activity city is required'),
+        venue: Yup.string().required('The activity venue is required'),
     })
 
     useEffect(() => {
@@ -59,16 +64,12 @@ export default observer(function ActivityForm() {
                 enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
                 {({ handleSubmit }) => (
                     <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
-                        <FormField>
-                            <Field placeholder='Title' name='title' />
-                            <ErrorMessage name="title" render={error => <Label basic color="red" content={error}></Label>} />
-                        </FormField>
-                        <Field placeholder='Title' name='title' />
-                        <Field placeholder='Description' name='description' />
-                        <Field placeholder='Category' name='category' />
-                        <Field type='date' placeholder='Date' name='date' />
-                        <Field placeholder='City' name='city' />
-                        <Field placeholder='Venue' name='venue' />
+                        <MyTextInput name='title' placeholder="Title" />
+                        <MyTextInput placeholder='Description' name='description' />
+                        <MyTextInput placeholder='Category' name='category' />
+                        <MyTextInput type='date' placeholder='Date' name='date' />
+                        <MyTextInput placeholder='City' name='city' />
+                        <MyTextInput placeholder='Venue' name='venue' />
                         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
