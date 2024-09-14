3293183ef227f47ecdb673f85028770ab66ac0fa
Boris Shulman WGT <boriss@waveguardtech.com>
2024-09-14 17:15:03 +0300

    Updating the home page and Nav

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,17 +1,31 @@
 import { Link } from "react-router-dom";
-import { Container, Header, Segment, Image, Button} from "semantic-ui-react";
+import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function HomePage() {
+export default observer(function HomePage() {
+    const { userStore } = useStore();
     return (
         <Segment inverted textAlign="center" vertical className="masthead">
             <Container text>
                 <Header as='h1' inverted>
-                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}}></Image>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }}></Image>
                     Reactivities
                 </Header>
-                <Header as='h2' inverted>Welcome to Reactivities</Header>
-                <Button as={Link} to='/login' color='teal' size='huge' inverted >Login!</Button>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as='h2' inverted>Welcome to Reactivities</Header>
+                        <Button as={Link} to='/activities' color='teal' size='huge' inverted >
+                            Go to Actvities!
+                        </Button>
+                    </>
+                ) : (
+                    <Button as={Link} to='/login' color='teal' size='huge' inverted >
+                        Login!
+                    </Button>
+
+                )}
             </Container>
         </Segment>
     )
-}
+})
