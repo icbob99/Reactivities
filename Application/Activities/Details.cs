474575b215b00e657902627d3eb4e5d86bdac46f
AzureAD\BorisShulman <boriss@waveguardtech.com>
2024-10-03 16:36:25 +0300

    Configure auto mapper

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,31 +1,38 @@
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Result<Activity>>
+        public class Query : IRequest<Result<ActivityDto>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Result<Activity>>
+        public class Handler : IRequestHandler<Query, Result<ActivityDto>>
         {
-            private readonly DataContext dataContext;
+            private readonly DataContext _dataContext;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext dataContext)
+            public Handler(DataContext dataContext, IMapper mapper)
             {
-                this.dataContext = dataContext;
+                this._dataContext = dataContext;
+                this._mapper = mapper;
             }
 
-            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var acttivity = await dataContext.Activities.FindAsync(request.Id);
+                var acttivity = await _dataContext.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync(a => a.Id == request.Id);
 
-                return Result<Activity>.Success(acttivity);
+                return Result<ActivityDto>.Success(acttivity);
             }
         }
     }
